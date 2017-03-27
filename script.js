63333c265feb3d8c396f33a6b6f60df78e3b73fe
AniKaTon <braintosha@yandex.ru>
2017-03-27 12:46:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 window.onload = function browser() 
 { 
-let ua = navigator.userAgent; 
-if (ua.search(/MSIE/) > -1) alert (':-('); 
-else alert('Поздравляем у вас не IE!'); 
+function getInternetExplorerVersion()
+                            {
+                                var rv = -1;
+                                if (navigator.appName == 'Microsoft Internet Explorer')
+                                {
+                                    var ua = navigator.userAgent;
+                                    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+                                    if (re.exec(ua) != null)
+                                        rv = parseFloat( RegExp.$1 );
+                                }
+                                else if (navigator.appName == 'Netscape')
+                                {
+                                    var ua = navigator.userAgent;
+                                    var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+                                    if (re.exec(ua) != null)
+                                        rv = parseFloat( RegExp.$1 );
+                                }
+                                return rv;
+}
+
+if (getInternetExplorerVersion() == -1) alert('Поздравляем, у Вас не IE!'); else alert(':-('); 
 }
